cd2e91e36ad7923467570f480faa2004ba21ba9f
Ben Balter <ben@balter.com>
2020-12-28 17:56:07 -0500

    header and footer

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,5 @@
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/collapse';
 import "bootstrap-table";
 import "bootstrap-table/dist/bootstrap-table.min.css";
 import 'bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.min.js';
